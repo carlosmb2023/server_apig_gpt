5bac0dfcb5e2f49987f64cb8792ee15d14da9698
Carlos Alexandre Vieira <187917214+carlosmb2023@users.noreply.github.com>
2025-04-25 01:35:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 echo "🚀 Iniciando DAN-XBOX Server no Render..."
 
-# Corrige erro ENOENT criando diretório no SSD persistente
+# Garante que o diretório dos browsers do Playwright exista no disco persistente
 mkdir -p /mnt/data/ms-playwright
 
-# Inicializa o servidor FastAPI
+# Inicializa o servidor
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
