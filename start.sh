bdcaaca455c3573633d16400c5e09400ad3afdfb
Carlos Alexandre Vieira <187917214+carlosmb2023@users.noreply.github.com>
2025-04-25 00:52:50 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "🚀 Iniciando DAN-XBOX Server no Render..."
-uvicorn main:app --host 0.0.0.0 --port $PORT
+
+# Garante que o diretório dos browsers do Playwright exista
+mkdir -p /ms-playwright
+
+# Inicializa o servidor
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
