624ac1ee5633e02855745684acce6933b78709dd
Carlos Alexandre Vieira <187917214+carlosmb2023@users.noreply.github.com>
2025-04-25 02:58:37 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-echo "🚀 Iniciando DAN-XBOX Server no Render..."
-
-# 🧠 Diretório de cache Playwright já existe no SSD (/mnt/data/ms-playwright)
-# Não precisa mais criar ou linkar
-
-# 🧠 Exporta o path explicitamente (opcional)
 export PLAYWRIGHT_BROWSERS_PATH=/mnt/data/ms-playwright
-
-# 🚀 Starta servidor FastAPI
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
+uvicorn main:app --host 0.0.0.0 --port 10000
